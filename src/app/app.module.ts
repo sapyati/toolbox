0a1826eb3407382b3869fa4cdda6612bdf647e0d
sapyati <sapyati.smtm@gmail.com>
2018-10-30 16:49:15 +0530

    toolbox changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { AppComponent } from './app.component';
 import { RouterModule, Routes } from '@angular/router';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -7,6 +7,8 @@ import { MatButtonModule, MatCheckboxModule } from '@angular/material';
 import { MaterialModule } from '../app/material';
 import { AppslistComponent } from './appslist/appslist.component';
 import { LoginComponent } from './login/login.component';
+import {  CategoryPipe } from './appslist/bold.pipe';
+import { FormsModule } from '@angular/forms';
 const appRoutes: Routes = [
   {
     path: '',
@@ -20,16 +22,19 @@ const appRoutes: Routes = [
 @NgModule({
   declarations: [
     AppComponent,
+    CategoryPipe,
     AppslistComponent,
     LoginComponent
   ],
   imports: [
     RouterModule.forRoot(appRoutes),
     BrowserModule,
+    FormsModule,
     BrowserAnimationsModule,
     MaterialModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
 })
 export class AppModule { }
--- src/app/appslist/appslist.component.ts
+++ src/app/appslist/appslist.component.ts
@@ -1,26 +1,34 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormControl } from '@angular/forms';
+import { Observable } from 'rxjs';
+import {MatCardModule} from '@angular/material/card';
+import { CategoryPipe } from './bold.pipe';
 @Component({
   selector: 'app-appslist',
   templateUrl: './appslist.component.html',
-  styleUrls: ['./appslist.component.css']
+  styleUrls: ['./appslist.component.css'],
 })
-export class AppslistComponent implements OnInit {
-
+export class AppslistComponent {
+  records: Array<any>;
+  isDesc: boolean = false;
+  column: string = 'CategoryName';
   constructor() { }
 
   ngOnInit() {
-  }
-// -
-function expand() {
-  $(".search").toggleClass("close");
-  $(".input").toggleClass("square");
-  if ($('.search').hasClass('close')) {
-    $('input').focus();
-  } else {
-    $('input').blur();
+    this.records= [
+      { CategoryName: "Beverages", Description: "Coffees, teas" },
+      { CategoryName: "Condiments", Description: "Sweet and savory sauces" },
+      {  CategoryName: "Confections", Description: "Desserts and candies" },
+      { CategoryName: "Cheeses",  Description: "Smetana, Quark and Cheddar Cheese" },
+      { CategoryName: "Grains/Cereals", Description: "Breads, crackers, pasta, and cereal" },
+      {CategoryName: "Beverages", Description: "Beers, and ales" },
+      { CategoryName: "Condiments", Description: "Selishes, spreads, and seasonings" },
+      {CategoryName: "Confections", Description: "Sweet breads" },
+      {CategoryName: "Cheeses",  Description: "Cheese Burger" },
+      {CategoryName: "Grains/Cereals", Description: "Breads, crackers, pasta, and cereal" }
+     ];
+     // this.sort(this.column);
   }
 }
-$('button').on('click', expand);
 
-}
+
